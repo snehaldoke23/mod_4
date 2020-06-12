bb076a5c3f312ec0e2d2cbfdffb845c8abc7762e
Snehal Doke <47291317+snehaldoke23@users.noreply.github.com>
2020-06-12 17:31:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+(function(){
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (name in names) {
+
+   
+    var firstLetter = names[name].charAt(0).toLowerCase();
+    if (firstLetter==="j") {
+      byeSpeaker.speak(names[name]);
+    }
+    else {
+      helloSpeaker.speak(names[name]);
+    }
+  
+}
+})();
